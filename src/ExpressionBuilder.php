be7e3c3316f3542bb8bd20c09bab7d9d9aa5118a
Hendrik Legge <admin@themepoint.de>
2023-01-05 17:08:14 +0100

    Run cs fixer

--- src/ExpressionBuilder.php
+++ src/ExpressionBuilder.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 
 namespace Flexic\CronExpressionBuilder;
 
-
 final class ExpressionBuilder
 {
-
 }
--- src/Field/Field.php
+++ src/Field/Field.php
@@ -1,13 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2023 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 2.0.0
+ */
+
 namespace Flexic\CronExpressionBuilder\Field;
 
 use Cron\CronExpression;
+
 enum Field: int
 {
     case MINUTE = CronExpression::MINUTE;
+
     case HOUR = CronExpression::HOUR;
+
     case DAY = CronExpression::DAY;
+
     case MONTH = CronExpression::MONTH;
+
     case WEEKDAY = CronExpression::WEEKDAY;
 }
